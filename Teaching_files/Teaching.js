2cd6f697d12919934a940fa0ea2cfd0b4500852a
Ana Sokolova <sokolova.ana@gmail.com>
2016-09-24 07:00:14 +0200

    content copied

---
+++ Teaching_files/Teaching.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20131001
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Teaching_files/FormalShadow_01.png'),IWCreateImage('Teaching_files/FormalShadow_02.png'),IWCreateImage('Teaching_files/FormalShadow_03.png'),IWCreateImage('Teaching_files/FormalShadow_06.png'),IWCreateImage('Teaching_files/FormalShadow_12.png'),IWCreateImage('Teaching_files/FormalShadow_11.png'),IWCreateImage('Teaching_files/FormalShadow_10.png'),IWCreateImage('Teaching_files/FormalShadow_04.png')],null,2,0.700000,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Teaching_files/TeachingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');detectBrowser();adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
