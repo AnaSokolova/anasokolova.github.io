4fd0e8d043301c03c589f9d5a017ca48191cf5ac
Ana Sokolova <sokolova.ana@gmail.com>
2017-09-29 14:40:13 +0200

    update teaching pages..

--- Ana_Sokolova/FormaleSysteme2017_files/FormaleSysteme2017.js
+++ Ana_Sokolova/FormaleSysteme2017_files/FormaleSysteme2017.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20170928
+// Created by iWeb 3.0.4 local-build-20170929
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('FormaleSysteme2017_files/FormalShadow_01.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_02.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_03.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_06.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_12.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_11.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_10.png'),IWCreateImage('FormaleSysteme2017_files/FormalShadow_04.png')],null,2,0.700000,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000)});registry.applyEffects();}
