0cad0983e6a2653feb171d1dbca6157f6c416c54
Ana Sokolova <sokolova.ana@gmail.com>
2020-10-11 21:27:44 +0200

    PSAT games links

---
+++ PSAT2020_files/PSAT2020.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20201011
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('PSAT2020_files/FormalShadow_01.png'),IWCreateImage('PSAT2020_files/FormalShadow_02.png'),IWCreateImage('PSAT2020_files/FormalShadow_03.png'),IWCreateImage('PSAT2020_files/FormalShadow_06.png'),IWCreateImage('PSAT2020_files/FormalShadow_12.png'),IWCreateImage('PSAT2020_files/FormalShadow_11.png'),IWCreateImage('PSAT2020_files/FormalShadow_10.png'),IWCreateImage('PSAT2020_files/FormalShadow_04.png')],null,2,0.700000,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('PSAT2020_files/PSAT2020Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');detectBrowser();adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
